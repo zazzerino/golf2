a420eb85abb656447a0d447f7582d500f90810e0
Kyle Patterson <patterkyle@gmail.com>
2023-10-06 13:56:09 -0500

    Users module

---
+++ lib/golf/users.ex
@@ -0,0 +1,40 @@
+defmodule Golf.Users do
+  import Ecto.Query, warn: false
+
+  alias Golf.Repo
+  alias Golf.Users.{User, UserToken}
+
+  def get_user(user_id) do
+    Repo.get(User, user_id)
+  end
+
+  def get_user_by_token(token) do
+    from(t in UserToken,
+      where: [token: ^token],
+      join: u in assoc(t, :user),
+      select: u
+    )
+    |> Repo.one()
+  end
+
+  def get_username(user_id) do
+    from(u in User,
+      where: [id: ^user_id],
+      select: u.username
+    )
+    |> Repo.one()
+  end
+
+  def create_user() do
+    Repo.insert(%User{})
+  end
+
+  def insert_user_token(token) do
+    Repo.insert(token)
+  end
+
+  def update_username(user_id, new_name) do
+    from(u in User, where: [id: ^user_id])
+    |> Repo.update_all(set: [username: new_name])
+  end
+end
