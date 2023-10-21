c0fcb645f76f77032302604fbc336cbcd55d6e6a
Kyle Patterson <patterkyle@gmail.com>
2023-10-20 20:41:07 -0500

    release

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :golf, Golf.Repo,
   database: "golf_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  template: "template0"
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
